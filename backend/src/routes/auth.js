e822d3ad012f1f768561a2ee0b13f6f042fed356
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2025-01-23 11:46:16 +0530

    create api endpoints for signup and login

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,11 +1,86 @@
-import express from "express"
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { authenticate } from "../middleware/auth.js";
 
-const router = express.Router()
+const router = express.Router();
 
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      username,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user" });
+  }
+});
 
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-router.get("/",async(req,res)=>{
-    res.send("Hello World")
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    if (user.isBlocked) {
+      return res.status(403).json({ message: "Account is blocked" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+    // Set jwt in http only cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 10000, // * 24hours
+    });
+    res.status(200).json({
+      user: {
+        id: user._id,
+        username: user.username,
+        role: user.role,
+      },
+      message: "User Logged in successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in" });
+  }
+});
+
+router.post("/logout", authenticate, (req, res) => {
+  res.clearCookie("token");
+  res.json({ message: "Logged out successfully" });
+});
+
+router.get("/me",authenticate, async(req,res) =>{
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch (error) {
+    res.status(500).json({message:"Error fetching user data"})
+  }
 })
 
-export default router
+export default router;
